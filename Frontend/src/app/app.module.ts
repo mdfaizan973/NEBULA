93c5bb399541c60e3ef43a5a6ee92473bc7c8af3
mdfaizan973 <faizan.md9735@gmail.com>
2023-07-19 22:16:22 +0530

    able to make basicks

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ Frontend/src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  products: any[] = [];
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.http
+      .get<any[]>(
+        'https://hotelsapis.onrender.com/placesStore?_limit=10&_page=1'
+      )
+      .subscribe((response) => {
+        this.products = response;
+        console.log(response);
+      });
+  }
+}
