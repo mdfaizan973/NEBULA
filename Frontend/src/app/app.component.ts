02e205baa38e54d2dd2f61063517e61f9271d254
mdfaizan973 <faizan.md9735@gmail.com>
2023-07-20 01:22:52 +0530

    able to add hosts by FE

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { initFlowbite } from 'flowbite';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Frontend';
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; //-----
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -15,7 +15,7 @@ export class HomeComponent {
       )
       .subscribe((response) => {
         this.products = response;
-        console.log(response);
+        // console.log(response);
       });
   }
 }
