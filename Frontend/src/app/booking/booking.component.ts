9b73fe929454749df9a7e72cc59b0f7f24cfc9ae
mdfaizan973 <faizan.md9735@gmail.com>
2023-07-23 19:32:29 +0530

    done 90%

--- Frontend/src/app/booking/booking.component.ts
+++ Frontend/src/app/booking/booking.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
-  styleUrls: ['./booking.component.css']
+  styleUrls: ['./booking.component.css'],
 })
 export class BookingComponent {
-
+  constructor(private router: Router) {}
+  redirects() {
+    setTimeout(() => {
+      this.router.navigate(['/']);
+    }, 1600);
+  }
 }
--- Frontend/src/app/guest/guest.component.ts
+++ Frontend/src/app/guest/guest.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-guest',
   templateUrl: './guest.component.html',
-  styleUrls: ['./guest.component.css']
+  styleUrls: ['./guest.component.css'],
 })
 export class GuestComponent {
-
+  constructor(private router: Router) {}
+  redirect() {
+    setTimeout(() => {
+      this.router.navigate(['/booking']);
+    }, 1200);
+  }
 }
